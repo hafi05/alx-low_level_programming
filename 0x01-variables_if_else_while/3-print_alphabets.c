fe685be796d4c6b2faef98ac4d20bbe01d10d1dc
hafi05 <hafida.khalouki@edu.uca.ac.ma>
2023-04-24 06:38:55 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,12 +9,13 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+     char low;
 
-	for (i = 0; i < 52; i++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		putchar(alp[i]);
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+	        putchar(low)	
 	}
 	putchar('\n');
 	return (0);
