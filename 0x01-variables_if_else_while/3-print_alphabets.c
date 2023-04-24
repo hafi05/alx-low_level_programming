a4d3283e70509b1d6221ee006d8fe7853669cf22
hafi05 <hafida.khalouki@edu.uca.ac.ma>
2023-04-24 06:56:52 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-int main() {
-  char letter;
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-  // Print lowercase alphabet
-  for (letter = 'a'; letter <= 'z'; letter++) {
-    putchar(letter);
-  }
-
-  putchar('\n');
-
-  // Print uppercase alphabet
-  for (letter = 'A'; letter <= 'Z'; letter++) {
-    putchar(letter);
-  }
-
-  putchar('\n');
-
-  return 0;
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
