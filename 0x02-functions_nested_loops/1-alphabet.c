663892f9a4f53f2585e9bb81793f81c4269500d6
hafi05 <hafida.khalouki@edu.uca.ac.ma>
2023-04-26 13:59:15 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: prints_alphabets in lawercase
+ * Return: Always 0
+ */
+void print_alphabet(void);
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
